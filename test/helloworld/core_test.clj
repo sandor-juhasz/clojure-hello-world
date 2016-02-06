23ff18502b5931f17b68185ea152fcfb046b4bc0
sandor-juhasz <sandor.juhasz.1983@gmail.com>
2016-02-06 11:13:30 +0100

    Fixing tests.

--- test/helloworld/core_test.clj
+++ test/helloworld/core_test.clj
@@ -2,10 +2,6 @@
   (:require [clojure.test :refer :all]
             [helloworld.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 0))))
-
 (deftest myadd-test
   (testing "Addition"
-    (is 2 (my-add 1 1))))
+    (is (= 2 (my-add 1 1)))))
